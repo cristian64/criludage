316d58f076dfe4e9dec3ae7ad2e24a9a64949128
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2010-11-28 19:46:50 +0000

    Clases que abstraen de ActiveMQ. Proporcionan un par de métodos y no requiere ir cerrandose sesiones ni conexiones.

--- Criludage/Biblioteca Común/Consumidor.cs
+++ Criludage/Biblioteca Común/Consumidor.cs
@@ -3,9 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
 namespace Biblioteca_Común
 {
-    class Consumidor
+    public class Consumidor
     {
+        ConnectionFactory connectionFactory;
+        IConnection connection;
+        ISession session;
+        IDestination destination;
+        IMessageConsumer consumer;
+
+        Func<Object, Object> metodo;
+
+        /// <summary>
+        /// Crea un consumidor que recibe mensajes desde una cola o un topic indistintamente.
+        /// Cuando se recibe un mensaje se convierte a objeto y se ejecuta el método que recibe ese objeto.
+        /// </summary>
+        /// <param name="uri">Ruta al servicio de mensajería. Por ejemplo: tcp://localhost:61616</param>
+        /// <param name="destino">Nombre de la cola o del topic.</param>
+        /// <param name="metodo">Método que se ejecutará al recibir un mensaje. El método debe recibir un parámetro del tipo "object" y devolver "object".</param>
+        public Consumidor(string uri, string destino, Func<Object, Object> metodo)
+        {
+            connectionFactory = new ConnectionFactory(uri);
+            connection = connectionFactory.CreateConnection();
+            connection.Start();
+            session = connection.CreateSession();
+            destination = session.GetDestination(destino);
+            consumer = session.CreateConsumer(destination);
+            consumer.Listener += new MessageListener(OnMessage);
+            this.metodo = metodo;
+        }
+
+        /// <summary>
+        /// Método auxiliar que procesa los mensajes que se reciben.
+        /// Este método invoca el método indicado en el contructor de la clase.
+        /// </summary>
+        /// <param name="message">Mensaje que se ha recibido y que se va a procesar en el método.</param>
+        private void OnMessage(IMessage message)
+        {
+            ITextMessage objectMessage = message as ITextMessage;
+            metodo(objectMessage.ToObject());
+        }
+
+        /// <summary>
+        /// Finaliza la sesión y la conexión con el servicio de mensajería.
+        /// </summary>
+        ~Consumidor()
+        {
+            session.Close();
+            connection.Close();
+        }
     }
 }
--- Criludage/Biblioteca Común/Productor.cs
+++ Criludage/Biblioteca Común/Productor.cs
@@ -3,9 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
 namespace Biblioteca_Común
 {
-    class Productor
+    public class Productor
     {
+        ConnectionFactory connectionFactory;
+        IConnection connection;
+        ISession session;
+        IDestination destination;
+        IMessageProducer producer;
+
+        /// <summary>
+        /// Crea un productor que puede enviar mensajes a una cola o un topic indistintamente.
+        /// </summary>
+        /// <param name="uri">Ruta al servicio de mensajería. Por ejemplo: tcp://localhost:61616</param>
+        /// <param name="destino">Nombre de la cola o del topic.</param>
+        public Productor(string uri, string destino)
+        {
+            connectionFactory = new ConnectionFactory(uri);
+            connection = connectionFactory.CreateConnection();
+            connection.Start();
+            session = connection.CreateSession();
+            destination = session.GetDestination(destino);
+            producer = session.CreateProducer(destination);
+        }
+
+        /// <summary>
+        /// Envía un objeto que debe ser serializable en XML.
+        /// Véase XmlSerializer.
+        /// </summary>
+        /// <param name="objeto">Objeto que se va a enviar. Debe ser serializable en XML.</param>
+        public void Enviar(object objeto)
+        {
+            producer.Send(objeto);
+        }
+
+        /// <summary>
+        /// Finaliza la sesión y la conexión con el servicio de mensajería.
+        /// </summary>
+        ~Productor()
+        {
+            session.Close();
+            connection.Close();
+        }   
     }
 }
