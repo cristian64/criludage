892ffee882dde655d0aa029e81292dad68fc131e
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2010-12-29 21:48:45 +0000

    Un pequeño bucle en el main() del servicio. El bucle se detiene al pulsar la tecla Bloq Num. Algo así habrá que hacer luego.

--- Criludage/Servicio de Gestión de Compra/Program.cs
+++ Criludage/Servicio de Gestión de Compra/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Servicio_de_Gestión_de_Compra
 {
@@ -13,12 +14,19 @@ namespace Servicio_de_Gestión_de_Compra
             // Crear el topic dinamicamente y establecer los persmisos de los usuarios que pueden conectarse.
             // Arrancar el servicio web.
             // Arrancar página web.
-            // (aunque estas cosillas igual se hace mejor desde
+            // (aunque estas cosillas igual se hace mejor desde un programa en bash o tal...)
 
             // bucle principal:
             //      Comprobar qué solicitudes han finalizado pero todavía no se han enviado.
             //      Enviar e-mail a los talleres.
             //      Marcar la solicitud como finalizada.
+
+            bool valorInicial = System.Console.NumberLock;
+            while (System.Console.NumberLock == valorInicial)
+            {
+                System.Console.WriteLine("pollaca");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
