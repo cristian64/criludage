46f345f5bb9da4af9589b57b0d9ba76b8b7463ab
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2011-01-02 14:12:01 +0000

    Añadidos dos nuevos formularios para ver una solicitud y proponer una propuesta.

---
+++ Criludage/Aplicación de Taller/FormProponerPropuesta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Aplicación_de_Taller
+{
+    public partial class FormProponerPropuesta : UserControl
+    {
+        public FormProponerPropuesta()
+        {
+            InitializeComponent();
+        }
+    }
+}
