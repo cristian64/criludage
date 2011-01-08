65ea555c005eda5facbd89fdc0f4a7c28396346f
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2011-01-08 02:25:59 +0000

    Añadidas las clases Solicitud, Propuesta, Cliente y Desguace. Serán similares a las que hay en el proyecto de Escritorio, pero no exactamente iguales.
    Añadida también la base de datos al servicio web. De momento estará integrada con el proyecto hasta que haya que replicar.

---
+++ Criludage/Servicio de Gestión de Compra/Desguace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca_de_Servicio_de_Gestión_de_Compra
+{
+    public class Desguace
+    {
+    }
+}
---
+++ Criludage/Servicio de Gestión de Compra/bdglobal.sql
@@ -0,0 +1,71 @@
+﻿delete from propuestas;
+delete from solicitudes;
+delete from desguace;
+delete from cliente;
+
+drop table propuestas;
+drop table solicitudes;
+drop table desguace;
+drop table cliente;
+
+create table clientes
+(
+	id int identity(1,1),
+	usuario varchar(30) not null,
+	contrasena varchar(40) not null,
+	nombre varchar(50) not null,
+	nif varchar(50) not null,
+	correoElectronico varchar (50) not null,
+	direccion varchar(100) not null,
+	telefono varchar(100) not null,
+	informacionAdicional varchar(5000) not null,
+	constraint pk_clientes_id primary key (id),
+	constraint unq_clientes_usuario unique (usuario)
+);
+
+create table desguaces
+(
+	id int identity(1,1),
+	usuario varchar(30) not null,
+	contrasena varchar(40) not null,
+	nombre varchar(50) not null,
+	nif varchar(50) not null,
+	correoElectronico varchar (50) not null,
+	direccion varchar(100) not null,
+	telefono varchar(100) not null,
+	informacionAdicional varchar(5000) not null,
+	constraint pk_desguaces_id primary key (id),
+	constraint unq_desguaces_usuario unique (usuario)
+);
+
+create table solicitudes
+(
+	id int identity(1,1),
+	idCliente int not null,
+	descripcion varchar(5000) not null,
+	fecha datetime not null,
+	fechaEntrega datetime not null,
+	fechaRespuesta datetime not null,
+	precioMax float(23) not null,
+	negociadoAutomatico tinyint not null default 0,
+	estado varchar(50) not null,
+	informacionAdicional varchar(5000) not null,
+	constraint pk_solicitudes_id primary key (id),
+	constraint fk_solicitudes_idCliente foreign key (idCliente) references clientes (id)
+);
+
+create table propuestas
+(
+	id int identity(1,1),
+	idSolicitud int not null,
+	idDesguace int not null,
+	descripcion varchar(5000) not null,
+	fechaEntrega datetime not null,
+	precio float(23) not null,
+	estado varchar(50) not null,
+	informacionAdicional varchar(5000) not null,
+	foto image,
+	constraint pk_propuestas_id primary key (id),
+	constraint fk_propuestas_idSolicitud foreign key (idSolicitud) references solicitudes (id),
+	constraint fk_propuestas_idDesguace foreign key (idDesguace) references desguaces (id)
+);
