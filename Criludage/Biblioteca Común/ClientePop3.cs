e7a950f96c370cd8cc4488f97e41f42dc3e96976
jcz1710 <jcz1710@ef139444-a557-d80b-4018-ca74a06c2182>
2011-01-09 22:07:12 +0000

    Añadido el metodo ObtenerMensajesDesde(fecha) a la clase ClientePop3 que lee del servidor los correos recibidos a partir de una fecha. Para cada correo leido solo devuelve los asuntos y la fecha en la que se recibió.

--- Criludage/Biblioteca Común/ClientePop3.cs
+++ Criludage/Biblioteca Común/ClientePop3.cs
@@ -28,5 +28,43 @@ namespace Biblioteca_Común
 			// Autenticacion
 			cliente.Authenticate(usuario, contraseña);
         }
+
+        /// <summary>
+        /// Devuelve el asunto y fecha de los correos recibidos a partir de una fecha.
+        /// </summary>
+        /// <param name="fecha">Fecha a partir de la cual se leen los correos.</param>
+        /// <returns>Lista donde cada posicion contiene un asunto y una fecha pertenecientes a un correo.</returns>
+        public ArrayList ObtenerMensajesDesde(DateTime fecha)
+        {
+            ArrayList mensajes = new ArrayList();
+
+            int n = cliente.GetMessageCount();
+            bool buscar = true;
+
+            // El bucle se recorre desde n hasta 1 y mientras no se pase de fecha.
+            while (buscar && n > 0)
+            {
+                OpenPop.Mime.Header.MessageHeader m = cliente.GetMessageHeaders(n);
+
+                DateTime fechaMensaje = DateTime.Parse(m.Date);
+                if (fechaMensaje > fecha)
+                {
+                    ArrayList mensaje = new ArrayList(); // Array local
+                    mensaje.Add(fechaMensaje);
+                    mensaje.Add(m.Subject);
+
+                    mensajes.Add(mensaje); // Array global
+                }
+                else
+                {
+                    buscar = false;
+                }
+
+                n--;
+            }
+
+            // Se crea el array a devolver
+            return mensajes;
+        }
     }
 }
