05f51e000c1589af4985c227dfe14506cda421d2
jcz1710 <jcz1710@ef139444-a557-d80b-4018-ca74a06c2182>
2011-01-09 15:26:12 +0000

    Añadida la libreria para leer Pop3. La clase que se usa es Pop3Client

---
+++ Criludage/Biblioteca Común/OpenPop/lib/InvalidLoginOrPasswordException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OpenPop.Pop3.Exceptions
+{
+	/// <summary>
+	/// Thrown when either the username or the password is not accepted by the POP3 server.
+	/// </summary>
+	/// <remarks>Should be used only when using <see cref="AuthenticationMethod.APOP"/>.</remarks>
+	public class InvalidLoginOrPasswordException : PopClientException
+	{
+		///<summary>
+		/// Creates a InvalidLoginOrPasswordException with the given message and InnerException
+		///</summary>
+		///<param name="message">The message to include in the exception</param>
+		///<param name="innerException">The exception that is the course of this exception</param>
+		public InvalidLoginOrPasswordException(string message, Exception innerException)
+			: base(message, innerException)
+		{ }
+	}
+}
---
+++ Criludage/Biblioteca Común/OpenPop/lib/PopServerException.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace OpenPop.Pop3.Exceptions
+{
+	/// <summary>
+	/// Thrown when the server does not return +OK to a command.<br/>
+	/// The server response is then placed inside.
+	/// </summary>
+	public class PopServerException : PopClientException
+	{
+		///<summary>
+		/// Creates a PopServerException with the given message
+		///</summary>
+		///<param name="message">The message to include in the exception</param>
+		public PopServerException(string message)
+			: base(message)
+		{ }
+	}
+}
