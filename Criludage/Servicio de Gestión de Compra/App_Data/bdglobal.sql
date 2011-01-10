d091b28033bbeaa5fb76c93b280e705df1a2546b
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2011-01-10 00:56:30 +0000

    Recargada la base de datos global para eliminar los desguaces y clientes que tenía insertados.

--- Criludage/Servicio de Gestión de Compra/App_Data/bdglobal.sql
+++ Criludage/Servicio de Gestión de Compra/App_Data/bdglobal.sql
@@ -69,5 +69,5 @@ create table propuestas
 	constraint fk_propuestas_idDesguace foreign key (idDesguace) references desguaces (id)
 );
 
-insert into desguaces (usuario,contrasena,nombre,nif,correoElectronico,direccion,telefono,informacionAdicional) values ('gitano','qwerty','killo','12345678','gitano@yopmail.com','descampao de la esquina','1111111111','No hay informacion adicional');
-insert into clientes (usuario,contrasena,nombre,nif,correoElectronico,direccion,telefono,informacionAdicional) values ('chamorro','qwerty','killo','12345678','gitano@yopmail.com','descampao de la esquina','1111111111','No hay informacion adicional');
+--insert into desguaces (usuario,contrasena,nombre,nif,correoElectronico,direccion,telefono,informacionAdicional) values ('gitano','qwerty','killo','12345678','gitano@yopmail.com','descampao de la esquina','1111111111','No hay informacion adicional');
+--insert into clientes (usuario,contrasena,nombre,nif,correoElectronico,direccion,telefono,informacionAdicional) values ('chamorro','qwerty','killo','12345678','gitano@yopmail.com','descampao de la esquina','1111111111','No hay informacion adicional');
