7002210ffec9b208500a7cd1951f1708215900bc
cristian64 <cristian64@ef139444-a557-d80b-4018-ca74a06c2182>
2011-03-20 03:14:04 +0000

    Eliminada una púa que era necesaria con SQL Server (para falsear y que pensara que el .mdf estaba en su mismo directorio).

--- Criludage/Visor de Registro/Program.cs
+++ Criludage/Visor de Registro/Program.cs
@@ -15,14 +15,6 @@ namespace Visor_de_Registro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            // Modificamos "DataDirectory" porque la BD está en otro directorio.
-            string mdf = Application.ExecutablePath;
-            mdf = mdf.Remove(mdf.LastIndexOf(@"\bin\"));
-            string mdf2 = mdf.Remove(mdf.LastIndexOf(@"\Visor de Registro"));
-            mdf2 += @"\Servicio de Gestión de Compra\App_Data";
-            AppDomain.CurrentDomain.SetData("DataDirectory", @mdf2);
-
             Application.Run(new VisorRegistro());
         }
     }
